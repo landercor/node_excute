4506421bd0bcf51dc1e05f14ce289f5ce066b474
Andres Asprilla <fabianasprilla034@gmail.com>
2026-06-05 16:06:23 +0000

    done file: server.js test

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
-  
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
